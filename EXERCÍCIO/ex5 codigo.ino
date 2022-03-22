98d2a4397028e5b3739c85898eec2adfb2bbba42
manoosaes1 <81782874+manoosaes1@users.noreply.github.com>
2022-03-22 18:50:25 -0300

    Update ex5 codigo.ino

--- EXERCÍCIO/ex5 codigo.ino
+++ EXERCÍCIO/ex5 codigo.ino
@@ -1,12 +1,14 @@
-//<span style="font-weight:400">ligando a ledinha</span>
 int botao = 2;
 int led = 4;
 bool estadoLed = 0;
+
 void setup()
 {
   pinMode(botao,INPUT_PULLUP);//definindo "INPUT"
   pinMode(led,OUTPUT);
 }
+
+
 void loop()
 {
  if(digitalRead(botao)== HIGH)//botao apertado
